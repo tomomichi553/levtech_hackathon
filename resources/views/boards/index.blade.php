cce9832fbc4de4dcc859cbc6f62f1877ebaed18d
tomomichi553 <yoshidatomomichi373@gmail.com>
2024-01-21 18:13:38 +0900

    デザイン作成

--- resources/views/boards/index.blade.php
+++ resources/views/boards/index.blade.php
@@ -1,28 +1,30 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('掲示板一覧') }}
-        </h2>
+        <div class="flex items-center">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('掲示板一覧') }}
+            </h2>
+            <a href='/boards/create' class="bg-black text-white font-bold mx-10 py-5 px-10 rounded-full shadow-xl hover:bg-gray-dark hover:text-white ">作成</a>
+        </div>
     </x-slot>
-        <h1>掲示板一覧</h1>
-        <a href='/boards/create'>作成</a>
+        
         <div class = 'boards'>
             @foreach ($boards as $board)
-                <div class = 'board'>
+                <div class = 'w-3/4 m-auto'>
 
-                    <h2 class='title'><a href="/boards/{{$board->id}}">{{ $board->name }}</a></h2>
+                    <h2 class='title'><a class="my-5 font-semibold text-xl text-gray-800 leading-tight" href="/boards/{{$board->id}}">{{ $board->name }}</a></h2>
 
-                    
-                    <div class="edit">
-                        <a href="/boards/{{ $board->id }}/edit">edit</a>
+                    <div class="flex items-center">
+                        <div class="edit">
+                            <a class="bg-blue-600 hover:bg-blue-500 text-white rounded px-4 py-2 mx-4" href="/boards/{{ $board->id }}/edit">編集</a>
+                        </div>
+    
+                        <form action="/boards/{{ $board->id }}" id="form_{{ $board->id }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button class="bg-red-600 hover:bg-red-500 text-white rounded px-4 py-2 mx-4" type="button" onclick="deletePost({{ $board->id }})">削除</button> 
+                        </form>
                     </div>
-
-                    <form action="/boards/{{ $board->id }}" id="form_{{ $board->id }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="button" onclick="deletePost({{ $board->id }})">delete</button> 
-                    </form>
-
 
 
                 </div>
--- resources/views/boards/show.blade.php
+++ resources/views/boards/show.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{$board->name}}
-        </h2>
+        <div class="flex items-center">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{$board->name}}
+            </h2>
+            <a href='/posts/create/{{ $board->id }}' class="bg-black text-white font-bold mx-10 py-5 px-10 rounded-full shadow-xl hover:bg-gray-dark hover:text-white ">新規投稿</a>
+        </div>
     </x-slot>
         <h1>投稿一覧</h1>
-        <div>
-            <p>掲示板：{{ $board->name }}</p>
-        </div>
-        <a href='/posts/create/{{ $board->id }}'>新規投稿</a>
+        
         <div>
             @foreach ($posts as $post)
                 <div style='border:solid 1px; margin-bottom: 10px;'>
@@ -18,12 +18,14 @@
                     <p>カテゴリー：{{ $post->category->name }}</a></p>
                     <p>本文：{{$post->body}}</p>
                     <p>ユーザー：{{$post->user->name}}</p>
-                    <p class="edit">[<a href="/posts/{{ $post->id }}/edit">編集</a>]</p>
-                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="button" onclick="deletePost({{ $post->id }})">削除</button> 
-                    </form>
+                    <div class="flex items-center">
+                        <p class="edit"><a class="bg-blue-600 hover:bg-blue-500 text-white rounded px-4 py-2 mx-4" href="/posts/{{ $post->id }}/edit">編集</a></p>
+                        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="button" class="bg-red-600 hover:bg-red-500 text-white rounded px-4 py-2 mx-4" onclick="deletePost({{ $post->id }})">削除</button> 
+                        </form>
+                    </div>
                 </div>
             @endforeach
         </div>
